e9dab00ee005aef2200e246efb81d3a20a80b1cd
Santhoshkumar-p <sanssoft12@gmail.com>
2017-01-17 21:36:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+var sum1 = 0;
+//Using for loop
+for (var i = 1; i <= 1000; i++) {
+	sum1 = sum1 + i;
+}
+console.log("In for loop the sum is:"+sum1);
+
+var sum2 = 0;
+var j = 0;
+//Using while loop
+while ( j <= 1000 ) {
+	sum2 = sum2 + j;
+	j++;
+}
+console.log("In while loop the sum is:" +sum2);
